c94c0c10121295040225b270fb3b8498023f6643
Karl Baker <karl.alec.baker@gmail.com>
2018-04-05 16:00:20 +0100

    Update example output

--- GovukRegistersApiClientNet.Console/Program.cs
+++ GovukRegistersApiClientNet.Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿using GovukRegistersApiClientNet.Factories;
 using GovukRegistersApiClientNet.Services;
 using System;
+using System.Linq;
 
 namespace GovukRegistersApiClientNet.ConsoleApp
 {
@@ -9,9 +10,15 @@ namespace GovukRegistersApiClientNet.ConsoleApp
         public static void Main(string[] args)
         {
             var client = RegisterClientFactory.CreateRegisterClientAsync("country", Enums.Phase.ReadyToUse, new InMemoryDataStore(), new RsfDownloadService(), new RsfUpdateService(new Sha256Service())).GetAwaiter().GetResult();
-            var records = client.GetRecords();
+            var records = client.GetRecords().ToList();
 
-            Console.WriteLine(records);
+            foreach (var record in records)
+            {
+                var entry = record.GetEntry();
+                var itemData = record.GetItem().Json;
+                Console.WriteLine($"{entry.EntryNumber} {entry.Key} {itemData["name"]}");
+            }
+
             Console.ReadLine();
         }
     }
