93066b9eb442982380b6060f1d70a5b49cdd4498
Karl Baker <karl.alec.baker@gmail.com>
2018-04-06 17:38:24 +0100

    Update UpdateAndPublish script to push script to Nuget

--- UpdateAndPublish.ps1
+++ UpdateAndPublish.ps1
@@ -1,5 +1,15 @@
-Set-Variable -Name "NugetApiKey" -Value "NUGET_API_KEY_HERE"
+param([string]$NugetApiKey)
+
+nuget setApiKey $NugetApiKey
 
 dotnet pack GovukRegistersApiClientNet -c Release --include-symbols
 dotnet pack GovukRegistersApiClientNet.Implementation -c Release --include-symbols
 
+(Get-Content "Directory.Build.props") | ForEach-Object{
+    if($_ -match "<Version>(.*)</Version>"){
+        $clientVersion = $matches[1]
+
+		nuget push .\GovukRegistersApiClientNet\bin\Release\GovukRegistersApiClientNet.$clientVersion.nupkg $NugetApiKey -Source https://api.nuget.org/v3/index.json
+		nuget push .\GovukRegistersApiClientNet.Implementation\bin\Release\GovukRegistersApiClientNet.Implementation.1.1.0.nupkg $NugetApiKey -Source https://api.nuget.org/v3/index.json
+	}
+}
